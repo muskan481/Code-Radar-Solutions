41c79757e6d7959cc92d7e889b5ce1e4a4bc3459
muskan481 <muskangarg7676@gmail.com>
2025-01-30 11:50:39 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int a,b;//sum=0,diff=0,mul=0,div=0;
-    scanf("%d %d",&a,&b);
-    char ch;
-    scanf("%c",&ch);
+    int a,b;
+    char ch; //sum=0,diff=0,mul=0,div=0;
+    scanf("%d %d %c",&a ,&b , &ch);
+    //char ch;
+    //scanf("%c",&ch);
     //sum=a+b;
     //diff=a-b;
     //mul=a*b;
