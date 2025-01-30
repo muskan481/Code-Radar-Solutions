22c80beb102acc8a82b5657c9e89d72f54c28633
muskan481 <muskangarg7676@gmail.com>
2025-01-30 11:57:12 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,10 +4,6 @@ int main() {
     int a,b;
     char ch; 
     scanf("%d %d %c ",&a ,&b , &ch);
-    //sum=a+b;
-    //diff=a-b;
-    //mul=a*b;
-   // div=int(a/b);
      if(ch=='+'){
         printf("%d", a+b);
     }
@@ -18,7 +14,8 @@ int main() {
         printf("%d", a*b);
     }
     else if(ch=='/'){
-        printf("%d", a/b );
+        float div = a/b;
+        printf("%d", int(div) );
     }
     else{
         printf("Error");
